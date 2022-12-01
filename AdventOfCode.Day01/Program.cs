418c7275ca195f71cea8ca21763f2e0f0de80831
Matt Chandler <mkchandler@gmail.com>
2022-12-01 00:18:46 -0600

    Day 1 solution

---
+++ AdventOfCode.Day01/Program.cs
@@ -0,0 +1,30 @@
+﻿string input = File.ReadAllText("input.txt");
+
+long PartOne()
+{
+    long highestCalories = input
+        .Split("\n\n")
+        .Select(elf => elf
+            .Split('\n', StringSplitOptions.RemoveEmptyEntries)
+            .Aggregate(0L, (acc, value) => acc + long.Parse(value)))
+        .Max();
+
+    return highestCalories;
+}
+
+long PartTwo()
+{
+    long highestThreeElves = input
+        .Split("\n\n")
+        .Select(elf => elf
+            .Split('\n', StringSplitOptions.RemoveEmptyEntries)
+            .Aggregate(0L, (acc, value) => acc + long.Parse(value)))
+        .OrderDescending()
+        .Take(3)
+        .Sum();
+
+    return highestThreeElves;
+}
+
+Console.WriteLine($"Part one: {PartOne()}");
+Console.WriteLine($"Part two: {PartTwo()}");
